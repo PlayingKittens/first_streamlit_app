31b95a8804cbad41aac22603bc8beeb96ffcb31e
PlayingKittens <124064932+PlayingKittens@users.noreply.github.com>
2023-01-31 12:50:23 -0600

    Update streamlit_app.py
    Changed "Build your own fruit smoothie" from header to text.

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,4 +11,4 @@ streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
 
 streamlit.header('Special Menu')
-streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+streamlit.text('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
