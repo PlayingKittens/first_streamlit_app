8fdc5f5e5e124c6a5c92dfdbb43d7d3078b57de7
PlayingKittens <124064932+PlayingKittens@users.noreply.github.com>
2023-01-31 12:51:16 -0600

    Update streamlit_app.py
    Removed "Special menu" and changed "Build Your Own Fruit Smoothie" back to header.

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,5 +10,4 @@ streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
 
-streamlit.header('Special Menu')
-streamlit.text('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
