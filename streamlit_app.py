52cb58a469c7b21b36927e6572a68e72b50ee1e6
PlayingKittens <124064932+PlayingKittens@users.noreply.github.com>
2023-02-01 14:32:49 -0600

    Update streamlit_app.py
    Added function and button to insert fruit into fruilt_load_list table. Probably won't work, but best guess.

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,6 +15,11 @@ def get_fruit_load_list():
         my_cur.execute("select * from fruit_load_list")
         return my_cur.fetchall()
 
+def insert_row_snowflake(new_fruit):
+    with mycnx.cursor() as my_cur:
+        my_cur.execut("insert into fruilt_load_list values(new_fruit)")
+        return "Thanks for adding " + new_fruit
+
 streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('Breakfast Menu')
@@ -58,9 +63,8 @@ if streamlit.button('Get Fruit Load List'):
     my_data_rows = get_fruit_load_list()
     streamlit.dataframe(my_data_rows)
 
-streamlit.stop()
-
-add_my_fruit = streamlit.text_input('What fruit would you like to add?', 'jackfruit')
-streamlit.write('Thanks for adding ', add_my_fruit)
-
-my_cur.execute("insert into fruit_load_list values ('from streamlit')")
+add_my_fruit = streamlit.text_input('What fruit would you like to add?')
+if streamlit.button('Add a Fruit to the List'):
+    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+    back_from_function = insert_row_snowflake(add_my_fruit)
+    streamlit.text(back_from_function)
