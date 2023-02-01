2d776e7537ce2586ec5b633dac8c5ce1edd79f69
PlayingKittens <124064932+PlayingKittens@users.noreply.github.com>
2023-02-01 13:48:43 -0600

    Update streamlit_app.py
    added connector to get current user data

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,3 +38,10 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # write your own comment - what does this do?
 # put the data in a frame to clarify what is shown
 streamlit.dataframe(fruityvice_normalized)
+
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("Select current_user(), current_account(), current_region()")
+my_data_row = my_cur.fetchone()
+streamlit.text("Hellow from Snowflake:")
+streamlit.text(my_data_row)
