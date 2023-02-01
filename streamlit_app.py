95cd6cfa1dc1a72670328837ed062ce2d711acab
PlayingKittens <124064932+PlayingKittens@users.noreply.github.com>
2023-02-01 14:34:26 -0600

    Update streamlit_app.py
    Typo in insert_row_snowflake function

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,7 +16,7 @@ def get_fruit_load_list():
         return my_cur.fetchall()
 
 def insert_row_snowflake(new_fruit):
-    with mycnx.cursor() as my_cur:
+    with my_cnx.cursor() as my_cur:
         my_cur.execut("insert into fruilt_load_list values(new_fruit)")
         return "Thanks for adding " + new_fruit
 
